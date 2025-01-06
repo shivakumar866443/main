c1b5400f378403fb21e38424462ab4281a0cff09
EASYSTEPIN\shivakumar.avula <shivakumar.avula@easystepin.com>
2025-01-06 15:44:01 +0530

    second comment

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ module.exports = {
       port: process.env.PORT || 3000,
     },
     db: {
-      uri: process.env.MONGO_URI || 'mongodb://localhost:27017/music_library',
+      uri: process.env.MONGO_URI || 'mongodb+srv://shivavision2025:Shivakumar@1234@cluster0.ti9zz.mongodb.net/',
     },
     jwt: {
       secret: process.env.JWT_SECRET || 'supersecretkey',
